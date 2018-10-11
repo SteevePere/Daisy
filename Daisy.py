391cbb1b0161b514012ba89e15c23b15842756f1
steeve <toto@example.com>
2018-10-11 20:39:28 +0200

    removed bdate

--- Daisy.py
+++ Daisy.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from flaskext.mysql import MySQL
 from flask import render_template
-from wtforms import Form, IntegerField, DateField, StringField, validators
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, DateField, StringField, validators
+from wtforms.validators import (DataRequired,Length)
 from flask_bootstrap import Bootstrap
+import datetime
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -12,6 +15,7 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Makaveli'
 app.config['MYSQL_DATABASE_DB'] = 'Daisy'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['SECRET_KEY'] = 'top secret key'
 mysql.init_app(app)
 
 conn = mysql.connect()
@@ -21,7 +25,7 @@ cursor = conn.cursor()
 
 def home():
 	all_employees = []
-	cursor.execute("SELECT first_name, last_name, emp_no, birth_date FROM employees")
+	cursor.execute("SELECT first_name, last_name, emp_no FROM employees")
 	rows = cursor.fetchall()
 	columns = [desc[0] for desc in cursor.description]
 	for row in rows:
@@ -29,22 +33,18 @@ def home():
 		all_employees.append(row)
 	return render_template('index.html', employees = all_employees),200
 
-class RegistrationForm(Form):
-	emp_no = IntegerField('Employee Number', [validators.Length(min=1, max=25)])
-	first_name = StringField('First Name', [validators.Length(min=1, max=25)])
-	last_name = StringField('Last Name', [validators.Length(min=1, max=25)])
-	birth_date = DateField('Birth Date', [validators.Length(min=1, max=25)])
+class RegistrationForm(FlaskForm):
+	first_name = StringField('First Name', validators=[DataRequired()])
+	last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=25)])
 
 @app.route('/add', methods=['GET', 'POST'])
 
 def add():
-    form = RegistrationForm(request.form)
-    return render_template('add.html', form=form),200
+	form = RegistrationForm(request.form)
+	if (request.method == 'POST'):
+			return (request.form['first_name'])
+	return render_template('add.html', form=form),200
 
-@app.route('/add_emp', methods=['POST'])
-
-def add_emp():
-	return (request.form['first_name'])
 
 #ERROR ROUTE
 @app.errorhandler(404)
