a933663552791cecc3a4600564ccf512f4a5fce2
Steeve <you@example.com>
2018-10-12 16:34:09 +0200

    delete, edit

--- Daisy.py
+++ Daisy.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 from flaskext.mysql import MySQL
 from flask import render_template
 from flask_wtf import FlaskForm
@@ -22,6 +22,7 @@ conn = mysql.connect()
 cursor = conn.cursor()
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/list', methods=['GET']) #REST
 
 def home():
 	all_employees = []
@@ -31,6 +32,8 @@ def home():
 	for row in rows:
 		row = dict(zip(columns,row))
 		all_employees.append(row)
+	if(request.path == '/list'): #REST
+			return jsonify({'code':200, 'message':'OK', 'Employees':all_employees}),200
 	return render_template('index.html', employees = all_employees),200
 
 class RegistrationForm(FlaskForm):
@@ -42,9 +45,42 @@ class RegistrationForm(FlaskForm):
 def add():
 	form = RegistrationForm(request.form)
 	if (request.method == 'POST'):
-			return (request.form['first_name'])
+		firstname = str(request.form['first_name'])
+		lastname = str(request.form['last_name'])
+		cursor.execute("SELECT MAX(emp_no) FROM employees")
+		top_emp = cursor.fetchall()
+		empno = top_emp[0][0] + 1
+		cursor.execute("INSERT INTO employees (emp_no, first_name, last_name) VALUES ((%s), (%s), (%s))",(empno, firstname, lastname))
+		conn.commit()
+		return redirect(url_for('home'))
 	return render_template('add.html', form=form),200
 
+@app.route('/confirm/<empno>', methods=['GET', 'POST'])
+
+def confirm(empno):
+	return render_template('confirm.html', empno=empno),200
+
+@app.route('/delete/<empno>', methods=['GET', 'POST'])
+
+def delete(empno):
+	if (request.method == 'POST'):
+		cursor.execute("DELETE FROM employees WHERE emp_no = (%s)",(empno))
+		conn.commit()
+	return redirect('/')
+
+@app.route('/edit/<firstname>/<lastname>', methods=['GET', 'POST'])
+
+def edit(firstname,lastname):
+	form = RegistrationForm(request.form)
+	# if (request.method == 'POST'):
+	# 	newfirstname = str(request.form['first_name'])
+	# 	newlastname = str(request.form['last_name'])
+	# 	oldlastname = lastname
+	# 	oldfirstname = firstname
+	# 	cursor.execute("UPDATE employees SET first_name = (%s), last_name = (%s) WHERE first_name = (%s) AND last_name = (%s))",(newfirstname, newlastname, oldfirstname, oldlastname))
+	# 	conn.commit()
+	# 	return redirect(url_for('home'))
+	return render_template('edit.html', firstname=firstname, lastname=lastname, form=form),200
 
 #ERROR ROUTE
 @app.errorhandler(404)
