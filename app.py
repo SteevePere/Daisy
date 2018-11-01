471af34cd87d409405e3857a643a655b877536d5
steeve <toto@example.com>
2018-11-01 17:47:09 +0100

    tests, yml

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-# -- IMPORTS --
+"""Main app server"""
 
-from flask import Flask, request, jsonify, redirect, url_for
+# pylint: disable=W0312
+# pylint: disable=C0301
+# pylint: disable=C0326
+# pylint: disable=C0325
+# pylint: disable=C0111
+# pylint: disable=C0103
+# pylint: disable=R0903
+# pylint: disable=W0621
+# pylint: disable=W0622
+# pylint: disable=W0613
+
+# -- IMPORTS --
+from flask import Flask, request, jsonify, redirect, url_for, render_template
 from flaskext.mysql import MySQL
-from flask import render_template
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, DateField, StringField, RadioField, validators
-from wtforms.validators import (DataRequired,InputRequired,Length)
+from wtforms import StringField
+from wtforms.validators import (DataRequired, Length)
 from flask_bootstrap import Bootstrap
-import datetime, time
 
 # -- INIT OBJECTS --
 
@@ -48,15 +58,39 @@ def home():
 	rows = cursor.fetchall()
 	columns = [desc[0] for desc in cursor.description]
 	for row in rows:
-		row = dict(zip(columns,row))
+		row = dict(zip(columns, row))
 		if (row["gender"] == "M"):
 			row["gender"] = "Mr."
 		else:
 			(row["gender"]) = "Ms."
 		all_employees.append(row)
 	if(request.path == '/list'): #REST
-			return jsonify({'code':200, 'message':'OK', 'Employees':all_employees}),200
-	return render_template('index.html', employees = all_employees),200
+			return jsonify({'code':200, 'message':'OK', 'Employees':all_employees}), 200
+	return render_template('index.html', employees = all_employees), 200
+
+@app.route('/search', methods=['GET', 'POST'])
+
+def search():
+	employees = []
+	message = ""
+	search = True
+	no_match = False
+	input = str(request.form.get("search", ""))
+	input = '%' + input + '%'
+	cursor.execute("SELECT first_name, last_name, emp_no, gender, birth_date FROM employees WHERE (last_name LIKE (%s) OR first_name LIKE (%s) OR emp_no LIKE (%s) OR birth_date LIKE (%s))", (input, input, input, input))
+	rows = cursor.fetchall()
+	columns = [desc[0] for desc in cursor.description]
+	for row in rows:
+		row = dict(zip(columns, row))
+		if (row["gender"] == "M"):
+			row["gender"] = "Mr."
+		else:
+			(row["gender"]) = "Ms."
+		employees.append(row)
+	if (employees == []):
+		no_match = True
+		message = "No employees match your search!"
+	return render_template('index.html', employees = employees, message = message, search = search, no_match = no_match), 200
 
 #ADD ENTRY
 @app.route('/add', methods=['GET', 'POST'])
@@ -71,43 +105,43 @@ def add():
 		cursor.execute("SELECT MAX(emp_no) FROM employees")
 		top_emp = cursor.fetchall()
 		empno = top_emp[0][0] + 1
-		cursor.execute("INSERT INTO employees (emp_no, gender, first_name, last_name, birth_date) VALUES ((%s), (%s), (%s), (%s), (%s))",(empno, gender, firstname, lastname, b_date))
+		cursor.execute("INSERT INTO employees (emp_no, gender, first_name, last_name, birth_date) VALUES ((%s), (%s), (%s), (%s), (%s))", (empno, gender, firstname, lastname, b_date))
 		conn.commit()
 		return redirect(url_for('home'))
-	return render_template('add.html', form=form),200
+	return render_template('add.html', form=form), 200
 
 #REST ADD
-@app.route('/create', methods=['GET','POST'])
+@app.route('/create', methods=['GET', 'POST'])
 
 def create():
 	if (request.method == 'POST'):
 		if (str(request.headers.get('Content-Type')) != 'application/x-www-form-urlencoded'):
-			return jsonify({'message':'Unsupported Media Type','code':415}),415
+			return jsonify({'message':'Unsupported Media Type', 'code':415}), 415
 		firstname = str(request.form['first_name'])
 		lastname = str(request.form['last_name'])
 		cursor.execute("SELECT MAX(emp_no) FROM employees")
 		top_emp = cursor.fetchall()
 		empno = top_emp[0][0] + 1
-		cursor.execute("INSERT INTO employees (emp_no, first_name, last_name) VALUES ((%s), (%s), (%s))",(empno, firstname, lastname))
+		cursor.execute("INSERT INTO employees (emp_no, first_name, last_name) VALUES ((%s), (%s), (%s))", (empno, firstname, lastname))
 		conn.commit()
-		return jsonify({'code':201, 'message':'Created'}),201
-	return jsonify({'code':404,'message': 'Not Found'}),404
+		return jsonify({'code':201, 'message':'Created'}), 201
+	return jsonify({'code':404, 'message': 'Not Found'}), 404
 
 #DELETE ENTRY
 @app.route('/delete/<empno>', methods=['GET', 'POST'])
 
 def delete(empno):
 	if (request.method == 'POST'):
-		cursor.execute("DELETE FROM employees WHERE emp_no = (%s)",(empno))
+		cursor.execute("DELETE FROM employees WHERE emp_no = (%s)", (empno))
 		conn.commit()
 	return redirect('/')
 
 #EDIT PAGE
 @app.route('/edit/<empno>/<gender>/<firstname>/<lastname>/<bdate>', methods=['GET', 'POST'])
 
-def edit(empno,gender,firstname,lastname,bdate):
+def edit(empno, gender, firstname, lastname, bdate):
 	form = RegistrationForm(request.form)
-	return render_template('edit.html', empno=empno, gender=gender, firstname=firstname, lastname=lastname, b_date=bdate, form=form),200
+	return render_template('edit.html', empno=empno, gender=gender, firstname=firstname, lastname=lastname, b_date=bdate, form=form), 200
 
 #MODIFY ENTRY
 @app.route('/modify/<empno>', methods=['GET', 'POST'])
@@ -118,7 +152,7 @@ def modify(empno):
 	newgender = str(request.form['gender'])
 	newbdate = str(request.form['birth_date'])
 	empno=empno
-	cursor.execute("UPDATE employees SET gender = (%s), first_name = (%s), last_name = (%s), birth_date = (%s) WHERE emp_no = (%s)",(newgender, newfirstname, newlastname, newbdate, empno))
+	cursor.execute("UPDATE employees SET gender = (%s), first_name = (%s), last_name = (%s), birth_date = (%s) WHERE emp_no = (%s)", (newgender, newfirstname, newlastname, newbdate, empno))
 	conn.commit()
 	return redirect(url_for('home'))
 
@@ -126,9 +160,4 @@ def modify(empno):
 @app.errorhandler(404)
 
 def not_found(error):
-	return jsonify({'code':404,'message': 'Not Found'}),404
-
-# -- APP ENGINE --
-
-if __name__ == '__main__':
-	app.run('0.0.0.0',port='5000')
+	return jsonify({'code':404, 'message': 'Not Found'}), 404
